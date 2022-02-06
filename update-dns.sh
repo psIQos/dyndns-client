6ae98efee53087ef0c839a34a6785656e0b5c7e0
Julian Mathis <julianmathis95@gmail.com>
2022-02-06 17:53:47 +0000

    initial commit

---
+++ update-dns.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#ipv6=$(ifconfig eth0 | grep -Po '(?<=inet6 )([0-9a-z]{1,4}:){7}[0-9a-z]{1,4}')
+#ipv4=$(ifconfig eth0 | grep -Po '(?<=inet )(\d{1,3}.){3}\d{1,3}')
+ipv4=$(curl -s https://checkipv4.dedyn.io)
+ipv6=$(curl -s https://checkipv6.dedyn.io)
+auth='Authorization: Basic ********'
+
+resp=$(curl -H "$auth" "https://dynupdate.no-ip.com/nic/update?hostname=********&myip=$ipv4,$ipv6")
+
+echo $resp
+
